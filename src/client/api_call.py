6dd5646b03312bff19ff29524d3b100274299aa0
JaredBaileyDuke <jared.l.bailey@duke.edu>
2025-12-10 12:04:28 -0500

    Add streamlit UI, works with api call

--- src/client/api_call.py
+++ src/client/api_call.py
@@ -1,8 +1,13 @@
 # src/client/api_call.py
-
 import requests
 
-BASE_URL = "http://172.28.163.77:8000"  # car01
+BASE_URL = "http://172.28.163.77:8000"  # default
+
+
+def set_base_url(url):
+    global BASE_URL
+    BASE_URL = url
+
 
 def move_car(x, y, psi):
     url = f"{BASE_URL}/move"
@@ -10,10 +15,12 @@ def move_car(x, y, psi):
     r = requests.post(url, json=payload)
     return r.status_code, r.json()
 
+
 def stop_car():
     url = f"{BASE_URL}/stop"
     r = requests.post(url)
     return r.status_code, r.json()
+
 
 def car_status():
     url = f"{BASE_URL}/status"
